b772f78aabfc29608c6c31b6ee609c81690aec78
Dheeraj2002kumar <dk8549644@gmail.com>
2026-06-01 01:06:34 +0530

    update Binary search Tree

--- src/Tree/BinarySearchTree.java
+++ src/Tree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import java.util.ArrayList;
+
 public class BinarySearchTree {
     static class Node{
         int data;
@@ -28,8 +30,80 @@ public class BinarySearchTree {
         System.out.print(root.data + " ");
         inorder(root.right);
     }
+
+    public static boolean search(Node root, int key){
+        if (root == null) return false;
+
+        if(root.data == key) return true;
+        else if (root.data > key) return search(root.left, key);
+        else return search(root.right, key);
+    }
+
+    public static Node delete(Node root, int val){
+        if(root == null) return null;
+        if(root.data > val) root.left = delete(root.left, val);
+        else if(root.data < val) root.right = delete(root.right, val);
+        else{ // root.data == val
+            // case 1
+            if(root.left == null && root.right == null) return null;
+
+            // case 2
+            if(root.left == null) return root.right;
+            else if(root.right == null) return root.left;
+
+            // case 3
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+
+        }
+        return root;
+    }
+
+    public static Node inorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return  root;
+    }
+
+    public static void printInRange(Node root, int X, int Y){
+        if (root == null) return;
+
+        if(root.data >= X && root.data <= Y){
+            printInRange(root.left, X, Y);
+            System.out.print(root.data + " ");
+            printInRange(root.right, X, Y);
+        } else if(root.data >= Y) {
+            printInRange(root.left, X, Y);
+        } else printInRange(root.right, X, Y);
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
+        if(root == null) return;
+        path.add(root.data);
+
+        // leaf
+        if(root.left == null && root.right == null){
+            printPath(path);
+        } else {
+            printRoot2Leaf(root.left, path);
+            printRoot2Leaf(root.right, path);
+        }
+        path.remove(path.size()-1);
+    }
+
+    public static void printPath(ArrayList<Integer> path){
+        for(int i = 0; i < path.size(); i++){
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        int[] values = {5, 1, 3, 4, 2, 7};
+//        int[] values = {5, 1, 3, 4, 2, 7};
+//        int[] values = {8, 5, 3, 1, 4, 6, 10, 14};
+        int[] values = {8, 5, 3, 10, 11, 14};
         Node root = null;
 
         for(int i = 0; i < values.length; i++){
@@ -38,5 +112,15 @@ public class BinarySearchTree {
 
         inorder(root);
         System.out.println();
+
+//        if(search(root, 1)) System.out.println("found");
+//        else System.out.println("not found");
+
+//        delete(root, 4);
+//        inorder(root);
+
+//        printInRange(root, 6, 10);
+
+        printRoot2Leaf(root, new ArrayList<>());
     }
 }
