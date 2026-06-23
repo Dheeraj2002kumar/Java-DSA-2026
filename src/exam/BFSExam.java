56c5d54adaf5c9b15871bdfefe87cd8289c32c7b
Dheeraj2002kumar <dk8549644@gmail.com>
2026-06-23 22:29:25 +0530

    add BFS

--- src/exam/BFSExam.java
+++ src/exam/BFSExam.java
@@ -1,4 +1,20 @@
 package exam;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFSExam {
+    public static void main(String[] args){
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+
+        System.out.println(queue);
+        while(!queue.isEmpty()){
+            System.out.print(queue.poll() + " ");
+        }
+    }
 }
