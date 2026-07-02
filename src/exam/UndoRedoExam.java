b931daf1895fb4ad546f31e01f6b965bfbb6d67b
Dheeraj2002kumar <dk8549644@gmail.com>
2026-07-02 18:09:18 +0530

    update the practice code

--- src/exam/UndoRedoExam.java
+++ src/exam/UndoRedoExam.java
@@ -1,4 +1,42 @@
 package exam;
 
+import java.util.Stack;
+
 public class UndoRedoExam {
+    static Stack<String> undoStack = new Stack<>();
+    static Stack<String> redoStack = new Stack<>();
+
+    public static void performAction(String action){
+        undoStack.push(action);
+        redoStack.clear();
+        System.out.println("Perform: " + action);
+    }
+
+    public static void undo(){
+        if(!undoStack.isEmpty()){
+            String action = undoStack.pop();
+            redoStack.push(action);
+            System.out.println("Undo: " + action);
+        } else System.out.println("Nothing to undo");
+    }
+
+    public static void redo(){
+        if(!redoStack.isEmpty()){
+            String action = redoStack.pop();
+            undoStack.push(action);
+            System.out.println("Redo: " + action);
+        } else System.out.println("Nothing to redo");
+    }
+
+    public static void main(String[] args){
+        performAction("Type A");
+        performAction("Type B");
+        performAction("Type C");
+        performAction("Type D");
+
+        undo();
+        undo();
+
+        redo();
+    }
 }
