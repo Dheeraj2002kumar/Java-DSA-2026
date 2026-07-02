53688d74fdc68a8f6023fe8d873825b40e139d58
Dheeraj2002kumar <dk8549644@gmail.com>
2026-07-02 18:09:18 +0530

    update the practice code

--- src/exam/linkedLIstExam.java
+++ src/exam/linkedLIstExam.java
@@ -1,4 +1,24 @@
 package exam;
 
+import java.util.LinkedList;
+
 public class linkedLIstExam {
+    public static void main(String[] args){
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println("LinkedList: " + list);
+
+        list.remove(Integer.valueOf(3));
+        System.out.println("LinkedList: " + list);
+
+        int target = 5;
+        if(list.contains(target)){
+            System.out.println(target + " is found");
+        } else System.out.println(target + " is not fount");
+    }
 }
